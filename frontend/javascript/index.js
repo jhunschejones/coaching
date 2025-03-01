c20127f1060d8ef03ccae3b6892046a7998f5e3e
jhunschejones <joshjones103@gmail.com>
2025-03-01 23:28:20 +0900

    Add shoelace

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,6 +1,15 @@
 import "$styles/index.css"
 import "$styles/syntax-highlighting.css"
 
+// Example Shoelace components. Mix 'n' match however you like!
+import "@shoelace-style/shoelace/dist/components/button/button.js"
+import "@shoelace-style/shoelace/dist/components/icon/icon.js"
+import "@shoelace-style/shoelace/dist/components/spinner/spinner.js"
+
+// Use the public icons folder:
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js"
+setBasePath("/shoelace-assets")
+
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}"
 
